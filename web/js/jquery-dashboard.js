7ef19e8c4d5247438d5408b5a43ad22e8d099aa1
rainble <sunryhfut@126.com>
2018-03-15 09:36:08 +0800

    test

--- web/js/jquery-dashboard.js
+++ web/js/jquery-dashboard.js
@@ -1,6 +1,47 @@
 $(document).ready(function(){
-    alert(1);
+    //alert(1);
     $("#shape196").click(function(){
         $("h1.page-header").hide();
+        $("[data-toggle='popover']").popover();
     });
+
+
+    // $("#shape9").mouseover(function() {
+    //     ({
+    //         html : true,
+    //         title: title(),
+    //         delay:{show:500, hide:1000},
+    //         content: function() {
+    //             return content();
+    //         }
+    //     });
+    //     alert(2);
+    // })
+
+    var labFullClosed = 1000;
+    var int = self.setInterval(ask(), 1000);
+
+    function ask() {
+        $.ajax({
+            url:"http://localhost:8080/labFullClosed",
+            type:"get",
+            async:false,
+            dataType:"jsonp",
+            jsonp:"callback",
+            jsonpCallback:"labFullClosed",
+            error: function (xhr, status, errorThrown) {
+                console.log( "Error: " + errorThrown );
+                console.log( "Status: " + status );
+                console.log( xhr );
+            },
+            success:function (json) {
+                //alert(json[0]);
+                labFullClosed = json[1];
+                console.log("ask once" + labFullClosed);
+            }
+        });
+    }
+
+
+
 });
