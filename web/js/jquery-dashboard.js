48e5108eea0656e09abfad03c4f84f43474acf8a
rainble <sunryhfut@126.com>
2018-04-08 14:03:58 +0800

    Added new display effects in meeting room.

--- web/js/jquery-dashboard.js
+++ web/js/jquery-dashboard.js
@@ -3,136 +3,144 @@ $(document).ready(function(){
     $("#shape196").click(function(){
         $("h1.page-header").hide();
         $("[data-toggle='popover']").popover();
-       
+
     });
 
     $("#shape198").click(function () {
         convertGateToClosed();
     })
 
-    var labFullClosed = 1000;
-    var temperature = "0.0";
-    var frontDoor = 1000;
-    var frontWindow = 1000;
-    var meetingRoom = 1000;
-    var serverRoom = 1000;
 
+//这三句话开始了轮询，后面的1000即为轮询时间间隔，这三个int值用来作为参数传入以停止轮询
     var int_ifLabFullClose = self.setInterval(askIfLabFullClosed, 1000);
     var int_temperature = self.setInterval(askTemperature, 1000);
     var int_somebodyAtRoom = self.setInterval(askSomebodyAtRoom, 1000);
-
-
-
-    function askIfLabFullClosed() {
-        $.ajax({
-            url:"http://localhost:8080/smartLab/iffullclosed",
-            type:"get",
-            async:false,
-            dataType:"jsonp",
-            jsonp:"callback",
-            jsonpCallback:"labFullClosed",
-            error: function (xhr, status, errorThrown) {
-                console.log( "Error: " + errorThrown );
-                console.log( "Status: " + status );
-                console.log( xhr );
-            },
-            success:function (json) {
-                labFullClosed = json[1];
-                frontDoor = json[3];
-                frontWindow = json[5];
-                meetingRoom = json[7];
-                serverRoom = json[9];
-                console.log("ask once: " + labFullClosed);
-                $(".labfullclosed").text(changeNumberToState(labFullClosed));
-                $(".frontDoor").text(changeNumberToState(frontDoor));
-                $(".frontWindow").text(changeNumberToState(frontWindow));
-                $(".meetingRoom").text(changeNumberToState(meetingRoom));
-                $(".serverRoom").text(changeNumberToState(serverRoom));
-                changeFrontDoor(frontDoor);
-                changeFrontWindow(frontWindow);
-                changeMeetingRoom(meetingRoom);
-                changeServerRoom(serverRoom);
-            }
-        });
-    }
-
-    function changeNumberToState(number) {
-        if (number == 1)
-            return "关";
-        else if (number == 0)
-            return "开";
-    }
-
-    function changeFrontDoor(frontDoor) {
-        if (frontDoor == 1)
-        {
-            convertGateToClosed();
-        }
-        else
-        {
-            convertGateToOpen();
-        }
-    }
-
-    function changeFrontWindow(frontWindow) {
-        if (frontWindow == 1)
-        {
-            convertWindowToClosed(10);
-        }
-        else
-        {
-            convertWindowToOpen(10);
-        }
-    }
-
-    function changeMeetingRoom(meetingRoom) {
-        if (meetingRoom == 1)
-        {
-            convertSideDoorToClosed(17);
-        }
-        else
-        {
-            convertSideDoorToOpen(17);
-        }
-    }
-
-    function changeServerRoom(serverRoom) {
-        if (serverRoom == 1)
-        {
-            convertSideDoorToClosed(18);
-        }
-        else
-        {
-            convertSideDoorToOpen(18);
-        }
-    }
-
-    function askTemperature() {
-        $.ajax({
-            url:"http://localhost:8080/smartLab/temperature",
-            type:"get",
-            async:false,
-            dataType:"jsonp",
-            jsonp:"callback",
-            jsonpCallback:"temperature",
-            error: function (xhr, status, errorThrown) {
-                console.log( "Error: " + errorThrown );
-                console.log( "Status: " + status );
-                console.log( xhr );
-            },
-            success:function (json) {
-                temperature = json[1];
-                console.log("ask once: " + temperature);
-                $(".temperature").text(temperature);
-            }
-        });
-    }
-
 });
 
-var somebodyAtRoom = 1000;
+//一些之后要用的变量
+var labFullClosed = 1000;
+var temperature = "0.0";
+var frontDoor = 1000;
+var frontWindow = 1000;
+var meetingRoom = 1000;
+var serverRoom = 1000;
 
-function askSomebodyAtRoom() {
+//询问温度的代码（格式一致）
+function askTemperature() {
+    $.ajax({
+        url:"http://localhost:8080/smartLab/temperature",//网址随意改
+        type:"get",
+        async:false,
+        dataType:"jsonp",
+        jsonp:"callback",
+        jsonpCallback:"temperature",//callback定义的字符串必须也要出现在服务器传过来的json字符串里面，还要在最开始，格式如"callback（json）"
+        error: function (xhr, status, errorThrown) {
+            console.log( "Error: " + errorThrown );
+            console.log( "Status: " + status );
+            console.log( xhr );
+        },
+        success:function (json) {
+            temperature = json[1];
+            console.log("ask once: " + temperature);
+            $(".temperature").text(temperature);
+        }
+    });
+}
+
+//询问门窗的代码（格式一致）
+function askIfLabFullClosed() {
+    $.ajax({
+        url:"http://localhost:8080/smartLab/iffullclosed",
+        type:"get",
+        async:false,
+        dataType:"jsonp",
+        jsonp:"callback",
+        jsonpCallback:"labFullClosed",
+        error: function (xhr, status, errorThrown) {
+            console.log( "Error: " + errorThrown );
+            console.log( "Status: " + status );
+            console.log( xhr );
+        },
+        success:function (json) {
+            labFullClosed = json[1];
+            frontDoor = json[3];
+            frontWindow = json[5];
+            meetingRoom = json[7];
+            serverRoom = json[9];
+            console.log("ask once: " + labFullClosed);
+            $(".labfullclosed").text(changeNumberToState(labFullClosed));
+            $(".frontDoor").text(changeNumberToState(frontDoor));
+            $(".frontWindow").text(changeNumberToState(frontWindow));
+            $(".meetingRoom").text(changeNumberToState(meetingRoom));
+            $(".serverRoom").text(changeNumberToState(serverRoom));
+            changeFrontDoor(frontDoor);
+            changeFrontWindow(frontWindow);
+            changeMeetingRoom(meetingRoom);
+            changeServerRoom(serverRoom);
+        }
+    });
+}
+
+//把返回的0和1改成开和关，更直观
+function changeNumberToState(number) {
+    if (number == 1)
+        return "关";
+    else if (number == 0)
+        return "开";
+}
+
+//根据返回的0和1更改平面图里前门的图样
+function changeFrontDoor(frontDoor) {
+    if (frontDoor == 1)
+    {
+        convertGateToClosed();
+    }
+    else
+    {
+        convertGateToOpen();
+    }
+}
+
+//根据返回的0和1更改平面图里前门窗户的图样
+function changeFrontWindow(frontWindow) {
+    if (frontWindow == 1)
+    {
+        convertWindowToClosed(10);
+    }
+    else
+    {
+        convertWindowToOpen(10);
+    }
+}
+
+//根据返回的0和1更改平面图里会议室门的图样
+function changeMeetingRoom(meetingRoom) {
+    if (meetingRoom == 1)
+    {
+        convertSideDoorToClosed(17);
+    }
+    else
+    {
+        convertSideDoorToOpen(17);
+    }
+}
+
+//根据返回的0和1更改平面图里机房门的图样
+function changeServerRoom(serverRoom) {
+    if (serverRoom == 1)
+    {
+        convertSideDoorToClosed(18);
+    }
+    else
+    {
+        convertSideDoorToOpen(18);
+    }
+}
+
+//询问会议室是否有人
+var somebodyAtRoom = 1000;//防止0和1的初始值看不出代码是否出错
+
+    function askSomebodyAtRoom() {
     $.ajax({
         url: "http://localhost:8080/smartLab/ifEmptyRoom",
         type: "get",
@@ -150,6 +158,7 @@ function askSomebodyAtRoom() {
             somebodyAtRoom = json[1];
             console.log("ask once: meeting room" + somebodyAtRoom);
             $(".somebodyAtRoom").text(somebodyAtRoom);
+            convertMeetingRoom();
         }
     });
 }
@@ -201,4 +210,27 @@ function convertSideDoorToClosed(doorId){
 function convertSideDoorToOpen(doorId){
     $("#shape"+doorId+" path:first-child").attr("d","M0,0L42,0L42,7.6L0,7.6L0,0zM0,0L0,7.6L-4,7.6L-4,0L0,0zM42,0L42,7.6L46,7.6L46,0L42,0zM42,0L42,-42L38,-42L38,0L42,0z");
     $("#shape"+doorId+" path:last-child").attr("d","M0,0C0,-23.2,18.8,-42,42,-42M0,0L0,7.6L-4,7.6L-4,0L0,0zM42,0L42,7.6L46,7.6L46,0L42,0zM42,0L42,-42L38,-42L38,0L42,0z");
+}
+
+/*
+动态的改变会议室的标志，当有人的时候在会议室左下角出现有人的标志
+ */
+function convertMeetingRoom() {
+    if (somebodyAtRoom == 0)
+    {
+        document.getElementById("shape4794").style.display = "none";
+        document.getElementById("shape4795").style.display = "none";
+        document.getElementById("shape4796").style.display = "none";
+        document.getElementById("shape4797").style.display = "none";
+        document.getElementById("shape4798").style.display = "none";
+    }
+    else
+    {
+        document.getElementById("shape4794").style.display = "inline";
+        document.getElementById("shape4795").style.display = "inline";
+        document.getElementById("shape4796").style.display = "inline";
+        document.getElementById("shape4797").style.display = "inline";
+        document.getElementById("shape4798").style.display = "inline";
+    }
+
 }
